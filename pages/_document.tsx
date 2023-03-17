9a09b8ca205beaf645c116a9ff6b16a6675ae8a8
operatiumsc <operatium.sc@gmail.com>
2023-03-18 02:03:19 +0700

    Add meta to doc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta http-equiv='cache-control' content='no-cache' />
+      <meta http-equiv='expires' content='0' />
+      <meta http-equiv='pragma' content='no-cache' />
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,14 @@ import Hero from "./components/hero";
 import Nav from "./components/nav";
 import Head from "next/head";
 
+
 export default function Home() {
   return (
     <div>
-       <Head>
+      <Head>
+
         <title>Charairat Swaengsap</title>
+
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
 
