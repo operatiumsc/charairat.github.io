eb3e09eb7eea2da9b39791b71cf92928f7cb30a6
operatiumsc <operatium.sc@gmail.com>
2023-03-18 02:08:06 +0700

    Update meta and build app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <meta http-equiv='cache-control' content='no-cache' />
-      <meta http-equiv='expires' content='0' />
-      <meta http-equiv='pragma' content='no-cache' />
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,10 @@ export default function Home() {
   return (
     <div>
       <Head>
-
         <title>Charairat Swaengsap</title>
-
+        <meta http-equiv='cache-control' content='no-cache' />
+        <meta http-equiv='expires' content='0' />
+        <meta http-equiv='pragma' content='no-cache' />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
 
