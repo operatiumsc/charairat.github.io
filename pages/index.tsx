979259befaef553e1dd8051073443d9410553508
opertiumsc <operatium.sc@gmail.com>
2023-03-17 16:54:38 +0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import About from './components/about'
+import HeroSection from './components/hero'
+
+export default function Home() {
+  return (
+  <div>
+   <head>
+   <title>Charairat Swaengsap</title>
+   </head>
+    <HeroSection />
+    <About/>
+  </div>
+
+  )
+}
