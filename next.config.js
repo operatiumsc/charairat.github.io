ed47103f13168ac64d6e9ca24a15981b119115cc
operatiumsc <operatium.sc@gmail.com>
2023-03-18 04:04:45 +0700

    Update next config

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   //reactStrictMode: true,
   assetPrefix: isProd ? 'https://operatiumsc.github.io/' : '',
-  // images: {
-  //   domains: ['images.unsplash.com'],
-  //   formats: ["image/webp"],
-  //   unoptimized: true,
-  // },
+  images: {
+    // domains: ['images.unsplash.com'],
+    // formats: ["image/webp"],
+    unoptimized: true,
+  },
   trailingSlash: true,
 }
 
