01c3a68f772c8e16eb94345ed90bb3b6d813fe28
operatiumsc <operatiumsc@users.noreply.github.com>
2023-03-29 03:47:23 +0000

    deploy: 7dacb51ed14e513c40e59020635efa9548626b36

---
+++ _next/static/QmFcgRPRAYJimHibBHO3l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/348-f36f3426095a56ba.js","static/chunks/pages/index-df6a041a433cfdec.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
